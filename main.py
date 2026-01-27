5e0a6cba5980b36bf81a0f53e922229ef29ffc2b
NGUYENDUCANHPH65820 <nguyenducanhdemo2@gmail.com>
2026-01-27 21:46:51 +0700

    fix main

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ while True:
     print("1. THÊM SẢN PHẨM")
     print("2. HIỂN THỊ SẢN PHẨM")
     print("3. TÌM SẢM PHẨM")
-    print("4. LƯU VÀ THOÁT")
+    print("4. THOÁT CHƯƠNG TRÌNH")
 
     choice = input(" CHỌN CHỨC NĂNG: ")
     
     if choice == "1":
         add_product(products)
+        save_products(FILENAME,products)
     elif choice == "2":  
         show_products(products)
     elif choice == "3":
         find_product(products)
-    elif choice == "4":
-        save_products(FILENAME,products)
+    elif choice == "4":       
         print("TẠM BIỆT !!!")
         break
     else:
